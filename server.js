d06845f356e69b78032e4b28fccfc79734c316b4
mohaa24 <msmathnan@gmail.com>
2022-08-06 11:58:26 +0530

    adding ftp

--- server.js
+++ server.js
@@ -175,3 +175,28 @@ const job = nodeCron.schedule("10 * * * * *", function jobYouNeedToExecute() {
  requestLeasePropertyData(date);
  
 });
+
+// ========================
+// FTP Server
+// ========================
+
+// Quick start, create an active ftp server.
+const FtpSrv = require('ftp-srv');
+
+const port=21;
+const ftpServer = new FtpSrv({
+  url: "ftp://fluffy-garbanzo.herokuapp.com/:" + port,
+  anonymous: true,
+});
+
+ftpServer.on('login', ({ connection, username, password }, resolve, reject) => { 
+    if(username === 'user' && password === 'user'){
+        return resolve({ root:"/" });    
+    }
+    return reject(new errors.GeneralError('Invalid username or password', 401));
+});
+
+ftpServer.listen().then(() => { 
+    console.log('Ftp server is starting...')
+});
+
