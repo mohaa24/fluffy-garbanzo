31b235ed20cea0485992a63f6b055bf1f7b9f70b
mohaa24 <msmathnan@gmail.com>
2022-09-08 13:04:02 +0530

    .

--- server.js
+++ server.js
@@ -194,11 +194,11 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 const token = "ozyttvptsnkpbvjfhogsvrtojytbptqeljwbyhyp";
 const key = "wljszq3Wsj8omYXJk6Aek9BdMQCE8ecF7aGmK9hI";
 const saleApi =
-  // "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale?publishedOnPortals=35646&sort=inserted&pagesize=5000&sortOrder=desc";
-  "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale?&publishedOnPortals=35646&sortOrder=asc&status=listing";
+  // "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale?publishedOnPortals=35646&sort=inserted&pagesize=5000&sortOrder=desc&status=listing";
+  "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale?&publishedOnPortals=35646&sort=inserted&sortOrder=desc&status=listing";
 const soldApi =
   // "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale/sold?sort=unconditional&sortOrder=desc";
-  "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale/sold?pagesize=50&sort=unconditional&sortOrder=desc&unconditionalSince=2020-06-24";
+  "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/sale/sold?pagesize=50&sort=inserted&sortOrder=desc&unconditionalSince=2020-06-24&publishedOnPortals=35646&&status=listing";
 
 const leaseApi =
   "https://ap-southeast-2.api.vaultre.com.au/api/v1.3/properties/residential/lease?sort=modified&sortOrder=desc";
